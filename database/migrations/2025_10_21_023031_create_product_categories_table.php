0de97c87094955bf8ffff3fa2b5ef0a0702c03a8
M hendra satria <162526343+HendraSatria@users.noreply.github.com>
2026-01-07 19:43:06 +0700

    feat: create product categories table

--- database/migrations/2025_10_21_023031_create_product_categories_table.php
+++ database/migrations/2025_10_21_023031_create_product_categories_table.php
@@ -9,13 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('product_categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->timestamps();
-        });
-    }
+        public function up(): void
+        {
+            Schema::create('product_categories', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('description');
+                $table->timestamps();
+            });
+        }
 };
